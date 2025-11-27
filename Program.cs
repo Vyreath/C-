d0f5d3169f5237d9d7022f7df85e0bcf678cb461
Vyreath <victorxpert97@gmail.com>
2025-11-26 19:27:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// char
+
+using System.Diagnostics.CodeAnalysis;
+
+char letra = 'a';
+char num = '2';
+
+Console.WriteLine(num);
+
+Console.WriteLine(letra);
+
+
+
+// string
+
+string GrupoDeLetras = "causaa";
+
+Console.WriteLine(GrupoDeLetras);
+
+// aceptan numeros enteros del quer mas acepta al que menos acepta y escrito de mayor a menor
+
+byte menor = 1;
+short CasiMenor = 2;
+int CasiMasAlto = 3;
+long Mayor = 4;
+
+
+
+Console.WriteLine(Mayor);
+Console.WriteLine(CasiMenor);
+Console.WriteLine(CasiMenor);
+Console.WriteLine(menor);
+
+
+
+//lo mismo pero con numeros reales
+
+
+double menor2 = 1;
+float CasiMeneor2  = 2;
+Decimal Mayor2 = 3;
+
+
+//**************************************************
+
+
+int EdadJuan = 4;
+int EdadPedro = 5;
+
+Console.WriteLine(EdadJuan + EdadPedro);
+int SumaDeEdades = EdadPedro + EdadJuan;
+Console.WriteLine(SumaDeEdades);
+
+
+//**************************************************
+
+Console.WriteLine("Dame la base del triangulo");
+
+double BaseDelTriangulo = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("la base del triangulo es: "+ BaseDelTriangulo);
+
+String TEXTO = "10";
+
+Double Numero = Convert.ToDouble(TEXTO);
+
+Console.WriteLine("La altura es: "+ Numero);
+
+Double Area = (BaseDelTriangulo * Numero);
+
+Console.WriteLine("El Area De tu Trriangulo es: "+ Area);
+
+
+
+
+
+
+
+  
+
+
