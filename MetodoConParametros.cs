8484de3b8a1314c27473e9c2ababec10644d5f40
Vyreath <victorxpert97@gmail.com>
2025-12-02 14:54:51 -0600

    Add MetodoConParametros.cs file

---
+++ MetodoConParametros.cs
@@ -0,0 +1,10 @@
+﻿
+void personas (string nombre, int edad, double altura)
+{
+    Console.WriteLine($"El nombre de la persona es: {nombre}");
+    Console.WriteLine($"La edad de la persona es: {edad}");
+    Console.WriteLine($"La altura de la persona es {altura}");
+}
+
+personas("pablo",19,1.90);
+personas("Axel", 67, 1.8);
