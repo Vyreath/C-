cf06f3e37dd64bd0ee9ab0eae28569079b25fc07
Vyreath <victorxpert97@gmail.com>
2025-12-02 14:44:36 -0600

    Add character and word counting methods

---
+++ EjercicioMetodoReturn.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Escribe un texto: ");
+string texto = Console.ReadLine();
+
+int ContarCaracteres(string pTexto)
+{
+    return pTexto.Length;
+}
+
+int ContarPalabras (string pTexto)
+{
+    string[] palabras = pTexto.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    foreach (string p in palabras)
+    {
+        Console.WriteLine(p);
+    }
+    return palabras.Length;
+}
+
+int caracteres = ContarCaracteres(texto);
+Console.WriteLine($"Total del caracteres: {caracteres}");
+
+int palabras = ContarPalabras(texto);
+Console.WriteLine($"Total del palabras: {palabras}");
+
