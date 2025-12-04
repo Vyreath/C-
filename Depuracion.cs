c4c5a8195fcedf13ca9d49edc731ed470f24de81
Vyreath <victorxpert97@gmail.com>
2025-12-04 09:41:42 -0600

    Create Depuracion.cs

---
+++ Depuracion.cs
@@ -0,0 +1,11 @@
+double a = 4.5;
+double b = 4.5;
+double c = suma(a,b);
+
+Console.WriteLine($"{a}+{b} = {c}");
+Console.WriteLine("Fin del programa");
+
+double suma (double a, double b)
+{
+    return a + b;
+}
